5c8ad8411630a6266081e967ee9ac39502e72fb3
Cristiano Betta <cbetta@gmail.com>
2013-01-27 20:00:03 +0000

    Adds basic installer for prime stack

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Primo
 
+A better, configurable, default (prime) Rails stack.
+
+## Usage
+
+```
+gem install primo
+
+primo app_name
+```
+
+This generates PostgreSQL/HAML/Rspec Rails app using [this template](https://github.com/cbetta/primo/blob/master/templates/prime.rb).
+
+
 ## Release notes
 
+* **0.0.2** Added simple installer for "Prime stack"
 * **0.0.1** Gem skeleton
 
 ## License
---
+++ templates/prime.rb
@@ -0,0 +1,55 @@
+# Setup PG
+gsub_file "Gemfile", "gem 'sqlite3'", "gem 'pg'"
+
+database_config = <<-EOF
+development:
+  adapter: postgresql
+  encoding: unicode
+  database: #{app_name}_dev
+
+test:
+  adapter: postgresql
+  encoding: unicode
+  database: #{app_name}_test
+EOF
+
+remove_file "config/database.yml"
+file "config/database.yml", database_config
+file "config/database.yml.example", database_config
+
+# Setup HAML
+gem "haml"
+gem_group :development do
+  gem "haml-rails"
+end
+
+remove_file "app/views/layouts/application.html.erb"
+file "app/views/layouts/application.html.haml", <<-EOF
+!!!
+%html
+  %head
+    %title #{app_name.humanize}
+    = stylesheet_link_tag :application, media: "all"
+    = javascript_include_tag :application
+    = csrf_meta_tags
+  %body
+    = yield
+EOF
+
+# Setup Rspec
+gem_group :development, :test do
+  gem "rspec-rails"
+end
+
+run "rm -rf test"
+run "rm -rf autotest"
+
+run "rails generate rspec:install"
+
+# Specify generators
+application <<-EOF
+    config.generators do |g|
+      g.template_engine     :haml
+      g.test_framework      :rspec
+    end
+EOF
